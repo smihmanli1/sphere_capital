9536074f3fbb8fa4c95b1543e15a8ab01b119307
Sercan Mihmanli <smihmanli@Sercans-MacBook-Air.local>
2022-12-23 16:40:22 -0500

    Ability to add multiple subplots

--- src/strategy_analysis/borsa_istanbul_pairs_trading/plot_correlations.py
+++ src/strategy_analysis/borsa_istanbul_pairs_trading/plot_correlations.py
@@ -2,7 +2,9 @@ import glob
 import json
 import sys
 from collections import defaultdict
-
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as ps
 
 def ingestCorrelations(correlationsDict, result):
     for mainCorrelation in correlationsDict:
@@ -24,6 +26,34 @@ def consolidateCorrelations(correlationsDir):
 
 
 allCorrelations = consolidateCorrelations(sys.argv[1])
+
+
+# for ticker1 in allCorrelations:
+#     for ticker2 in allCorrelations[ticker1]:
+#         fig = px.histogram(allCorrelations[ticker1][ticker2])
+#         fig.show()
+
+
+# fig = px.histogram(allCorrelations['BIST30']['ZPX30.F'], title='BIST30_ZPX30.F', nbins=20)
+# fig.show()
+
+
+hist1 = go.Histogram(x=allCorrelations['BIST30']['ZPX30.F'], nbinsx=20)
+hist2 = go.Histogram(x=allCorrelations['BIST30']['DJIST.F'], nbinsx=20)
+
+# Create a figure with two subplots
+fig = ps.make_subplots(rows=1, cols=2)
+
+# Add the first histogram to the first subplot
+fig.add_trace(hist1, row=1, col=1)
+
+# Add the second histogram to the second subplot
+fig.add_trace(hist2, row=1, col=2)
+
+# Display the figure
+fig.show()
+
+
 
 
 
