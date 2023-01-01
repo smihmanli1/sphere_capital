cd5bad47a1a7214d9015a4b73a775cb304927345
Sercan Mihmanli <smihmanli@Sercans-Air.fios-router.home>
2023-01-01 18:08:56 -0500

    Correlation generation works

--- src/strategy_analysis/borsa_istanbul_pairs_trading/generate_correlations.py
+++ src/strategy_analysis/borsa_istanbul_pairs_trading/generate_correlations.py
@@ -1,22 +1,38 @@
+
+import sys
+import pandas as pd
+import datetime
+from collections import defaultdict
+import json
+
 allEtfs = ['USDTR.F', 'ZPBDL.F', 'ZPX30.F', 'ZRE20.F', 'ZPT10.F', 'GLDTR.F', 'DJIST.F', 'Z30KE.F', 'GMSTR.F', 'Z30KP.F', 'ZTM15.F', 'Z30EA.F', 'ZPLIB.F', 'ZELOT.F', 'ZGOLD.F']
+baskets = [
+    'ZPBDL.F_underlying', 
+    'ZPX30.F_underlying', 
+    'ZRE20.F_underlying', 
+    'ZPT10.F_underlying', 
+    'GLDTR.F_underlying', 
+    'DJIST.F_underlying', 
+    'Z30KE.F_underlying', 
+    'GMSTR.F_underlying', 
+    'Z30KP.F_underlying', 
+    'ZTM15.F_underlying', 
+    'Z30EA.F_underlying', 
+    'ZPLIB.F_underlying', 
+    'ZELOT.F_underlying', 
+    'ZGOLD.F_underlying',
+    'BIST30_one_of_each']
 
 # Calculate each basket based on basket weights located in basketWeightsDir
 # and add each basket's trajectory into the pricesDataFrame.
 # Return pricesDataFrame.
 def addBasketColumns(basketWeightsDir, pricesDataFrame, baskets):
-    pass
-
+    #TODO: Implement
+    return pricesDataFrame
 
 def getAllBaskets():
-
-    etfUnderlyingBaskets = []
-    for etf in allEtfs:
-        etfUnderlyingBaskets.append(f'{etf}_underlying')
-
-    returned = etfUnderlyingBaskets + ['BIST30_one_of_each']
-    
+    returned = etfUnderlyingBaskets + baskets
     return returned
-
 
 def getCorrelations(pricesDataFrame, priceTimeseries):
     result = defaultdict(dict)
@@ -32,25 +48,28 @@ startDateString = sys.argv[2]
 endDateString = sys.argv[3]
 basketWeightsDir = sys.argv[4]
 
-currentDate = datetime.strptime(startDateString, '%Y-%m-%d')
-endDate = datetime.strptime(endDateString, '%Y-%m-%d')
+currentDate = datetime.datetime.strptime(startDateString, '%Y-%m-%d')
+endDate = datetime.datetime.strptime(endDateString, '%Y-%m-%d')
 
 while currentDate != endDate:
     currentDateString = currentDate.strftime('%Y-%m-%d')
     currentDatePricesFile = f"{intervalPricesDataDir}/{currentDateString}_interval_prices.csv"
-    pricesDf = pd.read_csv(currentDatePricesFile)
+    
+    try:
+        pricesDf = pd.read_csv(currentDatePricesFile)
+        pricesDf = addBasketColumns(basketWeightsDir, pricesDf, baskets)
 
-    allBaskets = getAllBaskets()
-    #TODO: Implement addBasketColumns
-    pricesDf = addBasketColumns(basketWeightsDir, pricesDf, allBaskets)
+        allArbitrageable = baskets + allEtfs
+        dailyPriceCorrelations = getCorrelations(pricesDf, allArbitrageable)
 
-    allArbitrageable = allBaskets + allEtfs
-
-    dailyPriceCorrelations = getCorrelations(pricesDf, allArbitrageable)
-
-    with open(f"{runDateString}_correlations.json", "w+") as outfile:
-        json.dump(dailyPriceCorrelations, outfile)
-
+        with open(f"{currentDateString}_correlations.json", "w+") as outfile:
+            json.dump(dailyPriceCorrelations, outfile)
+    except FileNotFoundError:
+        pass
+    # except:
+    #     print (f"Critical error occurred when generating correlations for {currentDate}")
+    finally:
+        currentDate += datetime.timedelta(days=1)
 
 
 # print ("Correlations: ")
