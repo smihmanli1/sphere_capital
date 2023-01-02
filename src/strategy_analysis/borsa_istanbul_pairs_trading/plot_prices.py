7097860c034e8a2fe93ca2f34a2cd50585082b91
Sercan Mihmanli <smihmanli@Sercans-Air.fios-router.home>
2023-01-01 23:36:15 -0500

    Additional pairs to test

--- src/strategy_analysis/borsa_istanbul_pairs_trading/plot_prices.py
+++ src/strategy_analysis/borsa_istanbul_pairs_trading/plot_prices.py
@@ -22,7 +22,7 @@ def addPriceChangeColumn(index, pricesDf):
 
     return pricesDf
 
-def calculateReturn(pricesDf, index1Name, index2Name):
+def calculateReturn(pricesDf, index1Name, index2Name, buyThreshold, sellThreshold):
 
     numberOfMinutes = len(pricesDf[index1Name])
     totalReturn = 1
@@ -48,11 +48,11 @@ def calculateReturn(pricesDf, index1Name, index2Name):
         #0.2
         #0.01
         lastPriceChangeDiff = priceChangeDiff
-        if priceChangeDiff >= 0.5:
+        if priceChangeDiff >= buyThreshold:
             bought = True
             boughtPrice = priceChangeDiff
 
-        if bought and priceChangeDiff <= 0.1:
+        if bought and priceChangeDiff <= sellThreshold:
             totalReturn *= 1 + (boughtPrice - priceChangeDiff)/100
             bought = False
 
@@ -61,7 +61,7 @@ def calculateReturn(pricesDf, index1Name, index2Name):
 
     return totalReturn
 
-def getAllPriceCharts(pricesDir, startDate, endDate, index1, index2, tradingStartTimeString, tradingEndTimeString, plotPriceThreshold):
+def getAllPriceCharts(pricesDir, startDate, endDate, index1, index2, tradingStartTimeString, tradingEndTimeString, plotPriceThreshold, buyThreshold, sellThreshold):
 
     currentDate = startDate
     returnedLineCharts = []
@@ -107,7 +107,7 @@ def getAllPriceCharts(pricesDir, startDate, endDate, index1, index2, tradingStar
             priceChangeDiff = pricesDf[f"{index1.name}_change"] - pricesDf[f"{index2.name}_change"]
             pricesDf["price_change_diff"] = priceChangeDiff
 
-            totalReturn *= calculateReturn(pricesDf, index1.name, index2.name)
+            totalReturn *= calculateReturn(pricesDf, index1.name, index2.name, buyThreshold, sellThreshold)
 
             priceChangeDiff = [min(i,plotPriceThreshold) for i in priceChangeDiff]
             priceChangeDiff = [max(i,-plotPriceThreshold) for i in priceChangeDiff]
@@ -135,18 +135,33 @@ startDate = datetime.datetime.strptime(startDateString, '%Y-%m-%d')
 endDate = datetime.datetime.strptime(endDateString, '%Y-%m-%d')
 
 worthwhileIndices = [
-                    # (Index('BIST30_one_each', weightsDir), Index('DJIST.F_underlying', weightsDir) ),
-                    # (Index('BIST30_one_each', weightsDir), Index('ZPX30.F_underlying', weightsDir) ),
-                    # (Index('BIST30_one_each', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
-                    # (Index('BIST30_one_each', weightsDir), Index('ZRE20.F_underlying', weightsDir) ),
-                    # (Index('BIST30_one_each', weightsDir), Index('ZTM15.F_underlying', weightsDir) ),
-                    # (Index('KRDMA.E', weightsDir), Index('KRDMD.E', weightsDir) ),
+                    (Index('BIST30_one_each', weightsDir), Index('DJIST.F_underlying', weightsDir) ),
+                    (Index('BIST30_one_each', weightsDir), Index('ZPX30.F_underlying', weightsDir) ),
+                    (Index('BIST30_one_each', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
+                    (Index('BIST30_one_each', weightsDir), Index('ZRE20.F_underlying', weightsDir) ),
+                    (Index('BIST30_one_each', weightsDir), Index('ZTM15.F_underlying', weightsDir) ),
+                    (Index('KRDMA.E', weightsDir), Index('KRDMD.E', weightsDir) ),
                     (Index('DJIST.F_underlying', weightsDir), Index('ZPX30.F_underlying', weightsDir) ),
                     (Index('Z30EA.F_underlying', weightsDir), Index('ZPX30.F_underlying', weightsDir) ),
                     (Index('Z30EA.F_underlying', weightsDir), Index('ZRE20.F_underlying', weightsDir) ),
                     (Index('DJIST.F_underlying', weightsDir), Index('ZTM15.F_underlying', weightsDir) ),
                     (Index('DJIST.F_underlying', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
                     (Index('ZTM15.F_underlying', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
+                    (Index('ZPX30.F_underlying', weightsDir), Index('ZTM15.F_underlying', weightsDir) ),
+                    (Index('ZPX30.F_underlying', weightsDir), Index('ZPLIB.F_underlying', weightsDir) ),
+                    (Index('ZPX30.F_underlying', weightsDir), Index('ZELOT.F_underlying', weightsDir) ),
+                    (Index('DJIST.F_underlying', weightsDir), Index('ZPLIB.F_underlying', weightsDir) ),
+                    (Index('DJIST.F_underlying', weightsDir), Index('ZELOT.F_underlying', weightsDir) ),
+                    (Index('ZTM15.F_underlying', weightsDir), Index('ZPLIB.F_underlying', weightsDir) ),
+                    (Index('ZTM15.F_underlying', weightsDir), Index('ZELOT.F_underlying', weightsDir) ),
+                    (Index('Z30EA.F_underlying', weightsDir), Index('ZPLIB.F_underlying', weightsDir) ),
+                    (Index('Z30EA.F_underlying', weightsDir), Index('ZELOT.F_underlying', weightsDir) ),
+                    (Index('ZPT10.F_underlying', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
+                    (Index('ZRE20.F_underlying', weightsDir), Index('ZTM15.F_underlying', weightsDir) ),
+                    (Index('ZRE20.F_underlying', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
+                    (Index('ZPBDL.F_underlying', weightsDir), Index('Z30EA.F_underlying', weightsDir) ),
+                    (Index('ZPBDL.F_underlying', weightsDir), Index('DJIST.F_underlying', weightsDir) ),
+                    (Index('ZPBDL.F_underlying', weightsDir), Index('ZRE20.F_underlying', weightsDir) ),
                     ]
 
 
@@ -156,7 +171,7 @@ for index1, index2 in worthwhileIndices:
     index1Name = index1.name
     index2Name = index2.name
 
-    allLineCharts = getAllPriceCharts(pricesDir, startDate, endDate, index1, index2, "10:15:00", "17:45:00", 0.5)
+    allLineCharts = getAllPriceCharts(pricesDir, startDate, endDate, index1, index2, "10:15:00", "17:45:00", 0.5, 0.5, 0.1)
 
     numColumns = 5
     fig = ps.make_subplots(rows=math.ceil(len(allLineCharts)/numColumns), cols=numColumns)
