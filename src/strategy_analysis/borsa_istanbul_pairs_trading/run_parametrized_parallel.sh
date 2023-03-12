0b129e2c2844f9e5a60a0f0e0e0d6ee6d0ff7592
Sercan <smihmanli@gmail.com>
2023-03-12 19:06:36 -0400

    Add a parallelizer script

---
+++ src/strategy_analysis/borsa_istanbul_pairs_trading/run_parametrized_parallel.sh
@@ -0,0 +1,23 @@
+
+# One day takes 15 mins
+# 20 days in a month.
+# 500 different runs
+# 150000 minutes = 104 days
+# Run on 30 cores --> 3.5 days
+
+
+runCommand=$1
+
+
+for buyThreshold in 0.05 0.01 0.02 0.03 0.04 0.06 0.07 0.08 0.09 0.10
+do
+    for sellThreshold in 0 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09
+    do
+        for positionLimit in 25000 50000 100000 150000 200000
+        do
+            echo "${runCommand} ${buyThreshold} ${sellThreshold} ${positionLimit} > result_buy_t${buyThreshold}_sell_t${sellThreshold}_pos_limit${positionLimit}.log&"
+            ${runCommand} ${buyThreshold} ${sellThreshold} ${positionLimit} > result_buy_t${buyThreshold}_sell_t${sellThreshold}_pos_limit${positionLimit}.log&
+        done
+        exit
+    done
+done
